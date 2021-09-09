0855f1cfdb20cdfa6ed4c2f8f7d499ade8a0cbb0
Ye013 <83494391+Ye013@users.noreply.github.com>
2021-09-10 05:13:53 +1000

    Add files via upload

---
+++ W3.js
@@ -0,0 +1,10 @@
+var txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var sln = txt.length;
+
+var x = 123;
+x.toString();
+(123).toString();
+(100+23).toString();
+
+var fruits = ["Banana", "Orange", "Apple", "Mango"];
+document.getElementById("P2").innerHTML = fruits.join(" * ");
